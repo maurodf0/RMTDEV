bfb3b07fc43b1c85d25a05f588b1974010e8d379
Mauro <maurodefalco96@gmail.com>
2024-01-04 00:50:34 +0100

    Refactoring with more components

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -3,8 +3,35 @@ import {
     jobDetailsContentEl,
     BASE_API_URL
 } from '../common.js';
+import renderJobDetails from './JobDetails.js';
 import renderSpinner from './Spinner.js';
 
+// create the blueprint of the function that we export and use into the search component
+const renderJobList = jobItemsArray => {
+    jobItemsArray.slice(0,7).forEach(jobItem => {
+        const newJobItemHTML = 
+        `<li class="job-item">
+            <a class="job-item__link" href="${jobItem.id}">
+                <div class="job-item__badge">${jobItem.badgeLetters}</div>
+                <div class="job-item__middle">
+                    <h3 class="third-heading">${jobItem.title}</h3>
+                    <p class="job-item__company">${jobItem.company}</p>
+                    <div class="job-item__extras">
+                        <p class="job-item__extra"><i class="fa-solid fa-clock job-item__extra-icon"></i> ${jobItem.duration}</p>
+                        <p class="job-item__extra"><i class="fa-solid fa-money-bill job-item__extra-icon"></i> ${jobItem.salary}</p>
+                        <p class="job-item__extra"><i class="fa-solid fa-location-dot job-item__extra-icon"></i> ${jobItem.location}</p>
+                    </div>
+                </div>
+                <div class="job-item__right">
+                    <i class="fa-solid fa-bookmark job-item__bookmark-icon"></i>
+                    <time class="job-item__time">${jobItem.daysAgo}</time>
+                </div>
+            </a>
+        </li>`
+    
+    jobListSearchEl.insertAdjacentHTML('beforeend', newJobItemHTML);
+    })    
+}
 
 const clickHandler = e => {
     e.preventDefault();
@@ -50,66 +77,13 @@ document.querySelector('.job-item--active')?.classList.remove('job-item--active'
 
 
     //render job details item
-
-    const jobDetailsHTML = 
-    `
-<img src="${jobItem.coverImgURL}" alt="#" class="job-details__cover-img">
-
-<a class="apply-btn" href="${jobItem.companyURL}" target="_blank">Apply <i class="fa-solid fa-square-arrow-up-right apply-btn__icon"></i></a>
-
-<section class="job-info">
-    <div class="job-info__left">
-        <div class="job-info__badge">${jobItem.badgeLetters}</div>
-        <div class="job-info__below-badge">
-            <time class="job-info__time">${jobItem.daysAgo}</time>
-            <button class="job-info__bookmark-btn">
-                <i class="fa-solid fa-bookmark job-info__bookmark-icon"></i>
-            </button>
-        </div>
-    </div>
-    <div class="job-info__right">
-        <h2 class="second-heading">${jobItem.title}</h2>
-        <p class="job-info__company">${jobItem.company}</p>
-        <p class="job-info__description">${jobItem.description}.</p>
-        <div class="job-info__extras">
-            <p class="job-info__extra"><i class="fa-solid fa-clock job-info__extra-icon"></i>${jobItem.duration}</p>
-            <p class="job-info__extra"><i class="fa-solid fa-money-bill job-info__extra-icon"></i> ${jobItem.salary}</p>
-            <p class="job-info__extra"><i class="fa-solid fa-location-dot job-info__extra-icon"></i> ${jobItem.Location}</p>
-        </div>
-    </div>
-</section>
-
-<div class="job-details__other">
-    <section class="qualifications">
-        <div class="qualifications__left">
-            <h4 class="fourth-heading">Qualifications</h4>
-            <p class="qualifications__sub-text">Other qualifications may apply</p>
-        </div>
-        <ul class="qualifications__list">
-           ${jobItem.qualifications.map(qualificationsText => `<li class="qualifications__item">${qualificationsText}</li>`
-            ).join('')}
-        </ul>
-    </section>
-
-    <section class="reviews">
-        <div class="reviews__left">
-            <h4 class="fourth-heading">Company reviews</h4>
-            <p class="reviews__sub-text">Recent things people are saying</p>
-        </div>
-        <ul class="reviews__list">
-            ${jobItem.reviews.map( reviewText => `<li class="reviews__item">${reviewText}</li>`).join('')}
-        </ul>
-    </section>
-</div>
-
-<footer class="job-details__footer">
-    <p class="job-details__footer-text">If possible, please reference that you found the job on <span class="u-bold">rmtDev</span>, we would really appreciate it!</p>
-</footer>`
-jobDetailsContentEl.innerHTML = jobDetailsHTML;
-
+    renderJobDetails(jobItem);
+    
 
      }).catch( error => console.log(error));
 
 }
 
 jobListSearchEl.addEventListener('click', clickHandler);
+
+export default renderJobList;
